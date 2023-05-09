88b2486b2adcc5d51c72fe338d9fc2e93316ab06
ashish <asisrautela612@gmail.com>
2023-05-10 00:50:18 +0530

    Initial commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../Styles/Header.css"
+
+const Header = () => {
+  return (
+     <nav>
+        Get Ready To Complete Your Daily Goals
+     </nav>
+  )
+}
+
+export default Header
